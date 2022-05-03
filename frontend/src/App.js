9d7f8022f3e48ad3c2d4b63e9600b113757c270d
tbollmeyer <tim.bollmeyer@fh-bielefeld.de>
2022-05-03 11:57:32 +0200

    Setup tile grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Tile from './components/Tile';
+
+import services from './data/services';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-gray-100 h-full md:h-screen w-full">
+      <section className="container mx-auto px-0 md:px-4 py-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-items-center gap-4">
+          {services.map((element, index) => (
+            <Tile 
+              key={index}
+              title={element.name}
+              link={element.link}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
 
