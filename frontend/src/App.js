04493162df56428e0f1ba6079f025e8dbf628578
tbollmeyer <tim.bollmeyer@fh-bielefeld.de>
2022-05-22 00:05:37 +0200

    Use header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,14 @@ import './App.css';
 import logo from './logo.png';
 import Tile from './components/Tile';
 import News from './components/News';
+import Header from './components/Header';
 
 import services from './data/services';
 
 function App() {
   return (
     <div>
-      <header className="flex items-center justify-center">
-        <h1 className="font-bold text-3xl md:text-5xl p-10">Herzlich Willkommen in</h1>
-        <img src={logo} className="mr-auto w-1/6 mt-2.5" alt="logo" />
-      </header>
+      <Header logo={logo}/>
       <main className="h-full md:h-screen w-full grid grid-cols-1 md:grid-cols-5">
         <section className="container mx-auto px-0 md:px-4 py-4 md:col-span-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-items-center gap-4">
