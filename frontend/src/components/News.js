a3ba3d6f90d2f91085c8917e354cf538c054c8f9
tbollmeyer <tim.bollmeyer@fh-bielefeld.de>
2022-05-21 23:29:53 +0200

    Change class to className

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -3,9 +3,9 @@ import './News.css';
 
 const News = ({ title, link, text }) => {
     return (
-        <a href={link} class="-m-3 p-3 rounded-lg bg-gray-100 hover:bg-gray-300">
-            <p class="text-base font-bold title-color text-center">{title}</p>
-            <p class="mt-1 text-sm text-gray-500">{text}</p>
+        <a href={link} className="-m-3 p-3 rounded-lg bg-gray-100 hover:bg-gray-300">
+            <p className="text-base font-bold title-color text-center">{title}</p>
+            <p className="mt-1 text-sm text-gray-500">{text}</p>
         </a>
     );
 }
